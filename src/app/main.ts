d0ebd27ea38b24ffcb1bb4d475d87cf9d6ab9c79
lgonzaloquijada <lgonzalo.quijada@gmail.com>
2025-02-12 19:14:18 -0700

    feat: mock and updateProduct.

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,17 +1,28 @@
-import { addProduct } from './products/product.service';
+import { addProduct, products } from './products/product.service';
+import { faker } from '@faker-js/faker';
 
-addProduct({
-  id: 1,
-  title: 'Camiseta',
-  stock: 100,
-  sizes: 'M',
-  category: {
-    id: 1,
-    name: 'Ropa',
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  },
-  createdAt: new Date(),
-  updatedAt: new Date(),
-});
+for (let i = 0; i < 10; i++) {
+  addProduct({
+    id: faker.string.uuid(),
+    title: faker.commerce.productName(),
+    stock: faker.number.int({ min: 1, max: 100 }),
+    sizes: faker.helpers.arrayElement(['S', 'M', 'L', 'XL']),
+    category: {
+      id: 1,
+      name: faker.commerce.department(),
+      createdAt: faker.date.recent(),
+      updatedAt: faker.date.recent(),
+    },
+    createdAt: faker.date.recent(),
+    updatedAt: faker.date.recent(),
+    color: faker.color.human(),
+    description: faker.commerce.productDescription(),
+    image: faker.image.url(),
+    isNew: faker.datatype.boolean(),
+    price: parseFloat(faker.commerce.price()),
+    tags: faker.helpers.arrayElements(['new', 'hot', 'sale', 'organic'], 2),
+  });
+}
+
 console.log(products);
+console.log(`Products count: ${products.length}`);
--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -6,3 +6,8 @@ export const addProduct = (product: Product) => {
   //product.id = products.length + 1; //we shouldn't do this in a real app
   products.push(product);
 };
+
+export const updateProduct = (id: string, product: Partial<Product>) => {
+  const index = products.findIndex((product) => product.id === id);
+  products[index] = { ...products[index], ...product };
+};
