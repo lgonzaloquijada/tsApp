ca94e0473a1483c400e83f36be06dfa59c318205
lgonzaloquijada <lgonzalo.quijada@gmail.com>
2025-02-12 18:37:13 -0700

    feat: base model, extending interfaces.

--- src/app/main.ts
+++ src/app/main.ts
@@ -3,12 +3,15 @@ import { addProduct } from './products/product.service';
 addProduct({
   id: 1,
   title: 'Camiseta',
-  createdAt: new Date(),
   stock: 100,
   sizes: 'M',
   category: {
     id: 1,
     name: 'Ropa',
+    createdAt: new Date(),
+    updatedAt: new Date(),
   },
+  createdAt: new Date(),
+  updatedAt: new Date(),
 });
 console.log(products);
