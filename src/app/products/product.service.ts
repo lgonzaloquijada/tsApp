56b192c3dd2281850ab5ad41004edb15d2466b17
lgonzaloquijada <lgonzalo.quijada@gmail.com>
2025-02-12 18:42:23 -0700

    feat: making id and createdAt readOnly.

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -3,5 +3,6 @@ import { Product } from './product.model';
 export const products: Product[] = [];
 
 export const addProduct = (product: Product) => {
+  //product.id = products.length + 1; //we shouldn't do this in a real app
   products.push(product);
 };
